ceed8f2fcb9f4d30da2b4e8f625c553d0c07a731
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-02-20 12:12:45 +0300

    Add class Notebook

--- Lesson_2/src/main/java/ru/prokhorov/Notebook.java
+++ Lesson_2/src/main/java/ru/prokhorov/Notebook.java
@@ -1,2 +1,33 @@
-package ru.prokhorov;public class Notebook {
+package ru.prokhorov;
+
+import java.math.BigDecimal;
+
+public class Notebook {
+    private BigDecimal price;
+    private int ram;
+    private String manufacturer;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
