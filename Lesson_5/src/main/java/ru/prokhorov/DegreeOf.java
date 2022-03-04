769a20ca57085a7ff99ab3f6bbc2bb99ff1518d5
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-03-04 09:31:04 +0300

    Add class degree

--- Lesson_5/src/main/java/ru/prokhorov/DegreeOf.java
+++ Lesson_5/src/main/java/ru/prokhorov/DegreeOf.java
@@ -1,4 +1,9 @@
 package ru.prokhorov;
 
 public class DegreeOf {
+
+    public double pow(double base, int degree){
+        if(degree == 1) return base;
+        return base * pow(base, degree-1);
+    }
 }
--- Lesson_5/src/main/java/ru/prokhorov/Main.java
+++ Lesson_5/src/main/java/ru/prokhorov/Main.java
@@ -2,6 +2,8 @@ package ru.prokhorov;
 
 public class Main {
     public static void main(String[] args) {
-        
+        DegreeOf degree = new DegreeOf();
+        double result = degree.pow(2.5, 3);
+        System.out.println(result);
     }
 }
