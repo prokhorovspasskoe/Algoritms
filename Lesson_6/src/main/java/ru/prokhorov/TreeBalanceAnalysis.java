090bccc1aa21343ab481aa38560d13dfb434eb02
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-03-08 11:43:00 +0300

    Determination of tree balance

--- Lesson_6/src/main/java/ru/prokhorov/TreeBalanceAnalysis.java
+++ Lesson_6/src/main/java/ru/prokhorov/TreeBalanceAnalysis.java
@@ -1,4 +1,16 @@
 package ru.prokhorov;
 
+import ru.prokhorov.Node;
+
 public class TreeBalanceAnalysis {
+    public boolean isBalanced(Node<Integer> node) {
+        return (node == null) ||
+                isBalanced(node.getLeftChild()) &&
+                        isBalanced(node.getRightChild()) &&
+                        Math.abs(height(node.getLeftChild()) - height(node.getRightChild())) <= 1;
+    }
+
+    private int height(Node<Integer> node) {
+        return node == null ? 0 : 1 + Math.max(height(node.getLeftChild()), height(node.getRightChild()));
+    }
 }
