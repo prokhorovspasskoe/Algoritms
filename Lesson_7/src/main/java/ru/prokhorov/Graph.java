c3ce899b94ecccb6072bd7931854b2080ba2ad8a
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-03-22 09:57:07 +0300

    Add lesson code

---
+++ Lesson_7/src/main/java/ru/prokhorov/Graph.java
@@ -0,0 +1,17 @@
+package ru.prokhorov;
+
+public interface Graph {
+    void addVertex(String label);
+
+    boolean addEdge(String startLabel, String secondLabel, String... others);
+    boolean addEdge(String startLabel, String secondLabel);
+
+    int getSize();
+
+    void display();
+
+    void dfs(String startLabel);
+
+    void bfs(String startLabel);
+
+}
