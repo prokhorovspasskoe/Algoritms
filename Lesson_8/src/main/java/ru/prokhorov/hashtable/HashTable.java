bee8fc9181985296a7dda425bc7cfef8b0ee29b9
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-03-27 08:29:50 +0300

    Add interface hashtable

---
+++ Lesson_8/src/main/java/ru/prokhorov/hashtable/HashTable.java
@@ -0,0 +1,22 @@
+package ru.prokhorov.hashtable;
+
+public interface HashTable<V, K> {
+    boolean put(K key, V value);
+
+    V get(K key);
+
+    V remove(K key);
+
+    int size();
+
+    boolean isEmpty();
+
+    void display();
+
+    interface Entry<K, V> {
+        K getKey();
+        V getValue();
+
+        void setValue(V value);
+    }
+}
